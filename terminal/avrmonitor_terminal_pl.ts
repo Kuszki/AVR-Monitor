06381db3f9e722ee297e0718228cac2e8457d5eb
Kuszki <l.drozdz@o2.pl>
2015-09-27 23:03:58 +0200

    Add terminal app translations

--- terminal/avrmonitor_terminal_pl.ts
+++ terminal/avrmonitor_terminal_pl.ts
@@ -7,34 +7,34 @@
         <location filename="avrdownloader.cpp" line="41"/>
         <source>Waiting 3 seconds for connection...
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Czekam 3 sekundy na połączenie...</translation>
     </message>
     <message numerus="yes">
         <location filename="avrdownloader.cpp" line="55"/>
         <source>Connected after %n milisecond(s)
 </source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Połączono po %n sekundzie\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
         </translation>
     </message>
     <message>
         <location filename="avrdownloader.cpp" line="65"/>
         <source>Device disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Urządzenie rozłączyło się</translation>
     </message>
     <message>
         <location filename="avrdownloader.cpp" line="73"/>
         <source>Master script downloaded
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Pobrano skrypt urządzenia\n\n</translation>
     </message>
     <message>
         <location filename="avrdownloader.cpp" line="86"/>
         <source>Cannot connect to device - connection timeout</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się połączyć z urządzeniem - minął czas oczekiwania</translation>
     </message>
 </context>
 <context>
@@ -43,27 +43,27 @@
         <location filename="avrterminal.cpp" line="44"/>
         <source>Waiting 3 seconds for connection...
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Czekam 3 sekundy na połączenie...</translation>
     </message>
     <message numerus="yes">
         <location filename="avrterminal.cpp" line="71"/>
         <source>Connected after %n milisecond(s)
 </source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Połączono po %n sekundzie\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
         </translation>
     </message>
     <message>
         <location filename="avrterminal.cpp" line="80"/>
         <source>Device disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Urządzenie rozłączyło się</translation>
     </message>
     <message>
         <location filename="avrterminal.cpp" line="88"/>
         <source>Cannot connect to device - connection timeout</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się połączyć z urządzeniem - minął czas oczekiwania</translation>
     </message>
 </context>
 <context>
@@ -72,50 +72,50 @@
         <location filename="avruploader.cpp" line="40"/>
         <source>Waiting 3 seconds for connection...
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Czekam 3 sekundy na połączenie...</translation>
     </message>
     <message numerus="yes">
         <location filename="avruploader.cpp" line="54"/>
         <source>Connected after %n milisecond(s)
 </source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Połączono po %n sekundzie\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
+            <numerusform>Połączono po %n sekundach\n</numerusform>
         </translation>
     </message>
     <message>
         <location filename="avruploader.cpp" line="65"/>
         <source>Uploading script from file...
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Wysyłanie skryptu do urządzenia...\n</translation>
     </message>
     <message>
         <location filename="avruploader.cpp" line="69"/>
         <source>Upload complete
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Transmisja zakończona\n</translation>
     </message>
     <message>
         <location filename="avruploader.cpp" line="71"/>
         <source>Can&apos;t open provided file</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się otworzyć wskazanego pliku</translation>
     </message>
     <message>
         <location filename="avruploader.cpp" line="77"/>
         <source>Device disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Urządzenie rozłączyło się</translation>
     </message>
     <message>
         <location filename="avruploader.cpp" line="85"/>
         <source>Cannot connect to device - connection timeout</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie udało się połączyć z urządzeniem - minął czas oczekiwania</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="main.cpp" line="46"/>
+        <location filename="main.cpp" line="74"/>
         <source>
 AVR-Terminal - terminal application for AVR-Monitor platform.
 
@@ -126,43 +126,58 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 Before use be sure that device is not runing or connected to another application.</source>
-        <translation type="unfinished"></translation>
+        <translation>AVR-Terminal - aplikacja terminala dla projektu AVR-Monitor.
+
+Copyright (C) 2015 Łukasz &quot;Kuszki&quot; Dróżdż - l.drozdz@o2.pl - https://github.com/Kuszki/AVR-Monitor.
+
+Niniejszy program jest wolnym oprogramowaniem – możesz go rozpowszechniać dalej i/lub modyfikować na warunkach Powszechnej Licencji Publicznej GNU wydanej przez Fundację Wolnego Oprogramowania, według wersji 3 tej Licencji lub dowolnej z późniejszych wersji.
+
+Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny – jednak BEZ ŻADNEJ GWARANCJI, nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. Bliższe informacje na ten temat można uzyskać z Powszechnej Licencji Publicznej GNU.
+
+Kopia Powszechnej Licencji Publicznej GNU powinna zostać ci dostarczona razem z tym programem. Jeżeli nie została dostarczona, odwiedź http://www.gnu.org/licenses/.
+
+Przed użyciem upewnij się, że inny program nie używa wybranego urządzenia.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="64"/>
+        <location filename="main.cpp" line="92"/>
         <source>device</source>
-        <translation type="unfinished"></translation>
+        <translation>urządzenie</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="64"/>
+        <location filename="main.cpp" line="92"/>
         <source>device to open</source>
-        <translation type="unfinished"></translation>
+        <translation>urządzenie do podłączenia</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="67"/>
-        <source>Become a TCP server (device works as master). [SID]</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.cpp" line="68"/>
+        <location filename="main.cpp" line="95"/>
         <source>Work as casual two-side terminal.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uruchamia klasyczny terminal.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="69"/>
+        <location filename="main.cpp" line="96"/>
         <source>Upload script from file into device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysyła wybrany plikt ze skryptem do urządzenia.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="69"/>
-        <location filename="main.cpp" line="70"/>
+        <location filename="main.cpp" line="96"/>
+        <location filename="main.cpp" line="97"/>
         <source>script</source>
-        <translation type="unfinished"></translation>
+        <translation>skrypt</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="70"/>
+        <location filename="main.cpp" line="97"/>
         <source>Download script from device into file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pobiera skrypt z urządzenia i zapisuje go do wskkazanego pliku.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="100"/>
+        <source>Cooperate with SQL server.</source>
+        <translation>Współpracuje z podanym serwerem SQL w celu udostępnienia interfejsu sieciowego.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="100"/>
+        <source>address</source>
+        <translation>adres</translation>
     </message>
 </context>
 </TS>
