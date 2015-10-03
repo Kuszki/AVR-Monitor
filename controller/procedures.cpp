2255e79945be19957659e64dc4015aa40865aa82
Kuszki <l.drozdz@o2.pl>
2015-10-03 20:02:34 +0200

    Merge PROGMEM constants into separate file

--- controller/procedures.cpp
+++ controller/procedures.cpp
@@ -19,46 +19,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "procedures.hpp"
-
-const char get_INFOSTR[] PROGMEM =
-"\n"
-"# AVR-Monitor device;   Controller program v1.6\n"
-"# Copyright (C) 2015     Łukasz \"Kuszki\" Dróżdż\n"
-"#\n"
-"# Info at https://github.com/Kuszki/AVR-Monitor\n"
-"#\n"
-"# Release date:            " __DATE__ " " __TIME__ "\n"
-"# Used tools:         GNU AVR-GCC version " __VERSION__ "\n"
-"# Built on:    Debian 9 GNU/Linux 4.2.1-2-amd64\n"
-"# GCC flags:    -Os -mmcu=atmega328p -std=c++11\n"
-"# Watchdog set:   on every evaluation for 8 sec\n"
-"#\n"
-"# Program size:        24348 bytes (74.3% Full)\n"
-"# Data size:             574 bytes (28.0% Full)\n"
-"\n";
-
-const char get_GAINS[] PROGMEM 	= { 0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111, 0b11110000 };
-
-const char get_EXPORT[] PROGMEM	= "export ";
-const char get_RETURN[] PROGMEM	= "return ";
-
-const char get_LINE[] PROGMEM		= "set LINE ";
-const char get_WORK[] PROGMEM		= "set WORK ";
-const char get_SHRD[] PROGMEM		= "set SHRD ";
-const char get_SHRE[] PROGMEM		= "set SHRE ";
-const char get_PGA0[] PROGMEM		= "set PGA0 ";
-const char get_PGA1[] PROGMEM		= "set PGA1 ";
-const char get_SLPT[] PROGMEM		= "set SLPT ";
-const char get_FRAM[] PROGMEM		= "set FRAM ";
-
-const char get_FAIL[] PROGMEM		= "fail ";
-const char get_CALL[] PROGMEM		= "call ";
-const char get_SET[] PROGMEM		= "set ";
-
-const char get_SOS[] PROGMEM		= SOS;
-const char get_EOS[] PROGMEM		= EOS;
-
-const char get_EOC[] PROGMEM		= EOC;
+#include "progmem.hpp"
 
 extern KAUart		UART;
 extern KASpi		SPI;
---
+++ controller/progmem.hpp
@@ -0,0 +1,67 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Program memory constans of AVR-Monitor UC program                      *
+ *  Copyright (C) 2015  Łukasz "Kuszki" Dróżdż            l.drozdz@o2.pl   *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the  Free Software Foundation, either  version 3 of the  License, or   *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This  program  is  distributed  in the hope  that it will be useful,   *
+ *  but WITHOUT ANY  WARRANTY;  without  even  the  implied  warranty of   *
+ *  MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the   *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have  received a copy  of the  GNU General Public License   *
+ *  along with this program. If not, see http://www.gnu.org/licenses/.     *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef _AVR_MONITOR_UC_PROGMEM
+#define _AVR_MONITOR_UC_PROGMEM
+
+#include "defines.hpp"
+#include "codes.hpp"
+
+const char get_INFOSTR[] PROGMEM =
+"\n"
+"# AVR-Monitor device;   Controller program v1.6\n"
+"# Copyright (C) 2015     Łukasz \"Kuszki\" Dróżdż\n"
+"#\n"
+"# Info at https://github.com/Kuszki/AVR-Monitor\n"
+"#\n"
+"# Release date:            " __DATE__ " " __TIME__ "\n"
+"# Used tools:         GNU AVR-GCC version " __VERSION__ "\n"
+"# Built on:    Debian 9 GNU/Linux 4.2.1-2-amd64\n"
+"# GCC flags:    -Os -mmcu=atmega328p -std=c++11\n"
+"# Watchdog set:   on every evaluation for 8 sec\n"
+"#\n"
+"# Program size:        24352 bytes (74.3% Full)\n"
+"# Data size:             574 bytes (28.0% Full)\n"
+"\n";
+
+const char get_GAINS[]	PROGMEM 	= { 0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111, 0b11110000 };
+
+const char get_EXPORT[]	PROGMEM	= "export ";
+const char get_RETURN[]	PROGMEM	= "return ";
+
+const char get_LINE[]	PROGMEM	= "set LINE ";
+const char get_WORK[]	PROGMEM	= "set WORK ";
+const char get_SHRD[]	PROGMEM	= "set SHRD ";
+const char get_SHRE[]	PROGMEM	= "set SHRE ";
+const char get_PGA0[]	PROGMEM	= "set PGA0 ";
+const char get_PGA1[]	PROGMEM	= "set PGA1 ";
+const char get_SLPT[]	PROGMEM	= "set SLPT ";
+const char get_FRAM[]	PROGMEM	= "set FRAM ";
+
+const char get_FAIL[]	PROGMEM	= "fail ";
+const char get_CALL[]	PROGMEM	= "call ";
+const char get_SET[]	PROGMEM	= "set ";
+
+const char get_SOS[]	PROGMEM	= SOS;
+const char get_EOS[]	PROGMEM	= EOS;
+
+const char get_EOC[]	PROGMEM	= EOC;
+
+#endif
